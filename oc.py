ade6166d0e2fe3afa6c3cfbc75bdce66e2dc53c1
jhancruz <61518750+jhancruz@users.noreply.github.com>
2020-03-02 11:42:54 -1000

    F. de unión y diferencia con conjuntos estáticos.

---
+++ oc.py
@@ -0,0 +1,11 @@
+conjunto1= {1,2,3,"df","b"}
+conjunto2={2,4,5,"c","b"}
+
+
+Uni= conjunto1 | conjunto2
+
+print("La union de los conjuntos es: ",Uni,"\n")
+
+dif= conjunto1 - conjunto2
+
+print("La diferencia de los conjuntos es: ",dif,"\n")
